89992f2fa0ec71ba9decd9b93cdee5452f518d1e
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2018-11-03 22:11:36 -0400

    Update Bot Framework/MainBotFile.py

--- Bot Framework/MainBotFile.py
+++ Bot Framework/MainBotFile.py
@@ -7,15 +7,8 @@ client = commands.Bot(command_prefix='$')
 
 @client.event
 async def on_ready():
+    await client.change_presence(game=discord.Game(name='Dope Rap Shit'))
     print('Rappy Rap Rap')
-
-
-@client.event
-async def on_message(message):
-    author = message.author
-    content = message.content
-    print('{}: {}'.format(author, content))
-    await client.process_commands(message)
 
 
 @client.command()
@@ -24,16 +17,22 @@ async def GrantMacdonald():
 
 
 @client.command()
-async def ShowmeSomeTravisScott():
-    await client.say('Heres some Travis Scott: https://www.youtube.com/watch?v=6ONRf7h3Mdk')
-
-
-@client.command()
-async def AstroWorldSickoMode():
+async def AstroWorld():
     await client.say('SICKO MODE, By Travis Scott: https://www.youtube.com/watch?v=d-JBBNg8YKs')
+    await client.say('Star Gazing, By Travis Scott: https://www.youtube.com/watch?v=2a8PgqWrc_4')
+    await client.say('WAKE UP, By Travis Scott: https://www.youtube.com/watch?v=FAO8ZAUBx0c')
+    await client.say('YOSEMITE, By Travis Scott: https://www.youtube.com/watch?v=ykMHDKB0-1o')
+    await client.say('HOUSTONFORNICATION, By Travis Scott: https://www.youtube.com/watch?v=XzmnM2PLPfs')
+    await client.say('NO BYSTANDERS, By Travis Scott: https://www.youtube.com/watch?v=OhXVLFpAAKY')
+    await client.say('Stop Trying to be God, By Travis Scott: https://www.youtube.com/watch?v=YqvCptqhHfs')
+    await client.say('CANT SAY, By Travis Scott: https://www.youtube.com/watch?v=gpnQhbOMQDA')
+    await client.say('WHO? WHAT!, By Travis Scott: https://www.youtube.com/watch?v=gLkQA7iLNUk')
+    await client.say('NC-17, By Travis Scott: https://www.youtube.com/watch?v=K2taklQnVzY')
 
 @client.command()
-async def AstroWorldStarGazing():
-    await client.say('Star Gazing, By Travis Scott: https://www.youtube.com/watch?v=2a8PgqWrc_4')
+async def AstroThunder():
+    await client.say('AstroThunder, By Travis Scott: https://www.youtube.com/watch?v=Pa67b28h0vY')
+
+
 
 client.run(TOKEN)
