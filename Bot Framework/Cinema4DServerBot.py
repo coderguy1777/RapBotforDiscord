793482a967777235c6303086243c1459e2cb1623
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2018-11-06 22:39:44 -0500

    Add files via upload

---
+++ Bot Framework/Cinema4DServerBot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+
+TOKEN = 'NTA5NDM5MzM4Njk5MDMwNTI4.DsN03w.KBzZWf1le4HdOtYDGioX0GIlrjQ'
+
+client = commands.Bot(command_prefix='%')
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=discord.Game(name='Cinema 4D'))
+    print('Cinema 4D, Duh.')
+
+
+@client.command()
+async def test():
+    await client.say('Hello Animator!')
+
+
+
+
+client.run(TOKEN)
--- Bot Framework/MainBotFile.py
+++ Bot Framework/MainBotFile.py
@@ -1,11 +1,10 @@
-from xmlrpc.client import boolean
-
 import discord
 from discord.ext import commands
 
 
 TOKEN = 'NTA4MTQyNTcxNDUzNjc3NTY4.Dr-tOw.gyvQANZjl0ujzpmA1CPz0n9XZOA'
 client = commands.Bot(command_prefix='$')
+
 
 @client.event
 async def on_ready():
@@ -34,25 +33,33 @@ async def Astroworld():
     await client.say('COFFEE BEAN, By Travis Scott: https://www.youtube.com/watch?v=Z6d3BofQqN0')
     await client.say('BUTTERFLY EFFECT, By Travis Scott: https://www.youtube.com/watch?v=k2pwvr8p4vM')
     await client.say('SKELETONS, By Travis Scott: https://www.youtube.com/watch?v=tAyYYKcySXA')
-    await client.say('SICKO MODE, By Travis Scott: https://www.youtube.com/watch?v=d-JBBNg8YKs')
-    await client.say('Star Gazing, By Travis Scott: https://www.youtube.com/watch?v=2a8PgqWrc_4')
-    await client.say('WAKE UP, By Travis Scott: https://www.youtube.com/watch?v=FAO8ZAUBx0c')
-    await client.say('YOSEMITE, By Travis Scott: https://www.youtube.com/watch?v=ykMHDKB0-1o')
-    await client.say('HOUSTONFORNICATION, By Travis Scott: https://www.youtube.com/watch?v=XzmnM2PLPfs')
-    await client.say('NO BYSTANDERS, By Travis Scott: https://www.youtube.com/watch?v=OhXVLFpAAKY')
-    await client.say('Stop Trying to be God, By Travis Scott: https://www.youtube.com/watch?v=YqvCptqhHfs')
-    await client.say('CANT SAY, By Travis Scott: https://www.youtube.com/watch?v=gpnQhbOMQDA')
-    await client.say('WHO? WHAT!, By Travis Scott: https://www.youtube.com/watch?v=gLkQA7iLNUk')
-    await client.say('NC-17, By Travis Scott: https://www.youtube.com/watch?v=K2taklQnVzY')
-    await client.say('5% TINT, By Travis Scott: https://www.youtube.com/watch?v=6SLD1ZQZ_4Y')
-    await client.say('COFFEE BEAN, By Travis Scott: https://www.youtube.com/watch?v=Z6d3BofQqN0')
-    await client.say('BUTTERFLY EFFECT, By Travis Scott: https://www.youtube.com/watch?v=k2pwvr8p4vM')
-    await client.say('SKELETONS, By Travis Scott: https://www.youtube.com/watch?v=tAyYYKcySXA')
+
+
+@client.command(pass_context=True)
+async def join(ctx):
+    author = ctx.message.author
+    channel = author.voice_channel
+    vc = await client.join_voice_channel(channel)
+
+@client.event
+async def play_yt(link):
+    player = await client.voice_client_in(client.get_server(serverID)).create_ytdl_player(link)
+    player.volume = 0.05
+    player.start()
+    return player
+
+    if message.content.startswith('!yt'):
+        link = message.content.strip('!yt ')
+        player = await play_yt(link)
+    
+@client.command()
+async def CANTSAY():
+    await client.say('https://www.youtube.com/watch?v=gpnQhbOMQDA')
 
 
 @client.command()
 async def AstroThunder():
-    await client.say('AstroThunder, By Travis Scott: https://www.youtube.com/watch?v=Pa67b28h0vY')
+    await client.say('https://www.youtube.com/watch?v=Pa67b28h0vY')
 
 
 @client.command()
